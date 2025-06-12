cbb361bbe7ca891e133cb0df683b38fa03fea220
bak3839 <bak3839@naver.com>
2025-06-12 22:20:29 +0900

    join() 관련 문제풀이

---
+++ src/thread/problem/section4/Problem2.java
@@ -0,0 +1,32 @@
+package thread.problem.section4;
+
+import static thread.util.ThreadUtils.sleep;
+import static util.MyLogger.log;
+
+public class Problem2 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(new Task(), "thread-1");
+        Thread thread2 = new Thread(new Task(), "thread-2");
+        Thread thread3 = new Thread(new Task(), "thread-3");
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        thread1.join();
+        thread2.join();
+        thread3.join();
+    }
+
+
+    static class Task implements Runnable {
+
+        @Override
+        public void run() {
+            for(int i = 1; i <= 3; i++) {
+                log(i);
+                sleep(1000);
+            }
+        }
+    }
+}
