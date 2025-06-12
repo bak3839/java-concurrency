671d8c7adb181f2743ec743b9cf2ff4942da95cd
bak3839 <bak3839@naver.com>
2025-06-12 16:07:28 +0900

    join()을 사용하여 스레드의 작업이 끝날 때까지 대기하기

---
+++ src/thread/control/join/JoinMainV3.java
@@ -0,0 +1,52 @@
+package thread.control.join;
+
+import static thread.util.ThreadUtils.sleep;
+import static util.MyLogger.log;
+
+public class JoinMainV3 {
+
+    public static void main(String[] args) throws InterruptedException {
+        log("start");
+        SumTask sumTask1 = new SumTask(1, 50);
+        SumTask sumTask2 = new SumTask(51, 100);
+        Thread thread1 = new Thread(sumTask1, "thread-1");
+        Thread thread2 = new Thread(sumTask2, "thread-2");
+
+        thread1.start();
+        thread2.start();
+
+        log("join() - main 스레드가 thread1, thread2 종료까지 대기");
+        thread1.join();
+        thread2.join();
+        log("join() - main 스레드 대기 완료");
+
+        log("task1.result: " + sumTask1.result);
+        log("task2.result: " + sumTask2.result);
+
+        int sumAll = sumTask1.result + sumTask2.result;
+        log("task1 + task2 = " + sumAll);
+        log("end");
+    }
+
+    static class SumTask implements Runnable {
+
+        int startValue = 0, endValue = 0, result = 0;
+
+        public SumTask(int startValue, int endValue) {
+            this.startValue = startValue;
+            this.endValue = endValue;
+        }
+
+        @Override
+        public void run() {
+            log("작업 시작");
+            sleep(2000);
+            int sum = 0;
+            for (int i = startValue; i <= endValue; i++) {
+                sum += i;
+            }
+            result = sum;
+            log("작업 완료 result = " + result);
+        }
+    }
+}
