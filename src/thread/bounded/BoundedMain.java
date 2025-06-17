155bfde7512c87b10bf0aa7594a6080aafdff338
bak3839 <bak3839@naver.com>
2025-06-17 22:49:35 +0900

    생산자 소비자 문제를 해결해주는 Blocking Queue 적용

--- src/thread/bounded/BoundedMain.java
+++ src/thread/bounded/BoundedMain.java
@@ -13,7 +13,8 @@ public class BoundedMain {
 //        BoundedQueue queue = new BoundedQueueV2(2);
 //        BoundedQueue queue = new BoundedQueueV3(2);
 //        BoundedQueue queue = new BoundedQueueV4(2);
-        BoundedQueue queue = new BoundedQueueV5(2);
+//        BoundedQueue queue = new BoundedQueueV5(2);
+        BoundedQueue queue = new BoundedQueueV6_1(2);
 
         // 2. 생산자, 소비자 실행 순서 선택, 반드기 하나만 선택!
         producerFirst(queue);
